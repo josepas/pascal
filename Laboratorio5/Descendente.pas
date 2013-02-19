a944ffe198d6fe33235b01f4cf2251fbb25029db
Jose Ricardo Pascarella Quijada <josepas@ramona.(none)>
2013-02-18 23:42:22 -0430

    Termine el Lab5

--- Laboratorio5/Descendente.pas
+++ Laboratorio5/Descendente.pas
@@ -11,9 +11,13 @@ program Lab3Ejercicio4;
 var
     a    : integer;
     b    : integer;
+    i    : integer;
     asc  : boolean;
     dsc  : boolean;    
-   
+    cotaAct : longint;
+    cotaPre : longint;
+    
+    
 begin
   
     writeln;
@@ -22,31 +26,61 @@ begin
     if a <= 0 then exit;
     read(b);
     if b <= 0 then exit;
-  
+    i := 0;
     asc := b > a;
     dsc := b < a;
-    {Precondicion: a > 0 /\ b > 0}
+    {Precondicion: 
+	a > 0 /\ b > 0
+    }
+    
+    {Cota: 
+	10000 - i
+    }
+    cotaAct := 10000 - i;
+       
     repeat
-	asc := b > a;
-	dsc := b < a;
+	if b > a then
+	begin
+	    asc := b > a;
+	end;
+	if b <= a then
+	begin
+	    dsc := b <= a;
+	end;
+	
+	i := i + 1;
+	
+	(* Chequeo de la Funcion de Cota *)
+	cotaPre := cotaAct;
+	cotaAct := 10000 - i;
+	if ((cotaAct < 0) or (cotaAct >= cotaPre)) then 
+	begin
+	    writeln('ERROR AL VERIFICAR LA FUNCION DE COTA.');
+	    writeln('POSIBILIDAD DE CICLO INFINITO.');
+	    halt;
+	end;
+	writeln('cotaAct= ', cotaAct);
+	
+	
 	a := b;
 	read(b);
     until (b <= 0);
     
     if asc = dsc then
-	begin
-	writeln('la sucesion es desordenada');
-	end
+    begin
+	writeln('la sucesion No es creciente');
+    end
     else if dsc then
-	begin
-	writeln('la sucesion es descendente');
-	end
+    begin
+	writeln('la sucesion NO es creciente');
+    end
     else 
-	begin
-	writeln('la sucesion es ascendente');
+    begin
+	writeln('la sucesion es creciente! :)');
     end;
-    {Postcondicion: asc = forall S(i) \: S(N) < S(N + 1)
-		    dsc = forall S(i) \: S(N + 1) < S(N)} 
+    {Postcondicion: 
+	asc == forall S(i) \ 1 <= i <= n : S(i) < S(i + 1)
+    }
     writeln;
     writeln;
 
--- Laboratorio5/NumeroPerfecto.pas
+++ Laboratorio5/NumeroPerfecto.pas
@@ -1,28 +1,69 @@
+(*
+ * NumeroPerfecto
+ *
+ * Programa que verifica si un numero N
+ * es un Natural Perfecto
+ *
+ * Autor:   Jose Pascarella
+ *
+ * Ultima modificacion: 18 / 02 / 2013
+ *)
+ 
 program NumeroPerfecto;
 
 var
-n         : integer;
-i         : integer;
-topeItera : integer;
-nPerfecto : integer;
-perfecto  : boolean;
+    n         : integer; // Numero Ingresado por el Usuario
+    i         : integer; // Variable contador
+    topeItera : integer; // Tope para las iteraciones
+    nPerfecto : integer; // Suma parcial de los divisores de N
+    perfecto  : boolean; // TRUE si el numero N es Perfecto
 
 begin
     writeln;
+    
+    (* Lectura de Datos *)
     write('Introduzca un numero natural: ');
     read(n);
     writeln;
     
+    (* Inicializacion de Variables *)
+    i := 1;
     topeItera := (n div 2) + 1;
     nPerfecto := 1;
     perfecto := false;
     
+    (* Verificacion de datos *)
+    while (n < 1) do
+    begin
+	writeln('Solo procesamos numeros naturales!');
+	write('Intente de nuevo: ');
+	read(n);
+	topeItera := (n div 2) + 1;
+	writeln;
+    end;
+    
+    {Precondicion:
+	n > 0
+    }
+    
+    {Invariante:
+	n > 0
+	/\ (2 <= i) /\ i <= (n / 2 + 1)
+	/\ nPerfecto = (*sigma i | 1 <= i <= (n / 2 + 1) : (n mod i = 0)) + 1
+    }
+    writeln('Tope= ', topeItera, '   N= ', n, '   Contador= ', i, '   Suma parcial= ', nPerfecto);
+    writeln;
+    
+    (* Calculos *)
     for i := 2 to topeItera do
     begin
 	if (n mod i = 0) then
 	begin
 	    nPerfecto := nPerfecto + i;
 	end;
+	
+	(* Verificacion del Invariante *)
+	writeln('Tope= ', topeItera, '   N= ', n, '   Contador= ', i, '   Suma parcial= ', nPerfecto);
     end;
     
     if nPerfecto = n then
@@ -30,6 +71,13 @@ begin
 	perfecto := true;
     end;
     
+    {Postcondicion:
+	nPerfecto = (*sigma i | 1 <= i <= (n / 2 + 1) : (n mod i = 0)) + 1
+	/\ perfecto == (nPerfecto = n)
+    }
+    
+    (* Escritura de Datos *)
+    writeln;
     if perfecto then
     begin
 	writeln('El numero introdcido (', n, ') es perfecto! :)');
@@ -38,5 +86,6 @@ begin
     begin
 	writeln('El numero introdcido (', n, ') NO es perfecto! ToT');
     end;
+    
     writeln;
 end.
