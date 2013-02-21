eb00434f535a4c2f66947742dadcb40dfaeb0a93
jose <jose@pancha.(none)>
2013-02-21 18:30:13 -0430

    Maratones fibonacci

---
+++ Maratones/Pruebas/FibonacciDP.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+    long long int* fmem;
+    
+    long long int fibonacci(long long int n) {
+	
+	if (n == 0) {
+	    return 0;
+	}
+	if (n == 1) {
+	    return 1;
+	}
+	if (fmem[n] != -1) {
+	    return fmem[n];
+	}
+	fmem[n-1] = fibonacci(n-1);
+	fmem[n-2] = fibonacci(n-2);
+	return fmem[n-1] + fmem[n-2];
+    }
+
+    int main() {
+	long long int n, h;
+	n = 40;
+	fmem = new long long int[n+1];
+	for (long long int i = 0; i <= n; i++) {
+	    fmem[i] = -1;
+	}
+	h = fibonacci(n);
+	return 0;
+    }
